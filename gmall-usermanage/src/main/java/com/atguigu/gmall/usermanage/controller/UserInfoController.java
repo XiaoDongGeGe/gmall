8ecaaa793fa59cf7a0b78d5b5dbe79bc40477399
XiaoDongGeGe <2287241256@qq.com>
2018-12-05 21:04:50 +0800

    补缺补漏

--- gmall-usermanage/src/main/java/com/atguigu/gmall/usermanage/controller/UserInfoController.java
+++ gmall-usermanage/src/main/java/com/atguigu/gmall/usermanage/controller/UserInfoController.java
@@ -3,11 +3,13 @@ package com.atguigu.gmall.usermanage.controller;
 import com.atguigu.gmall.bean.UserInfo;
 import com.atguigu.gmall.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@Controller
 public class UserInfoController {
     @Autowired
     private UserInfoService userInfoService;
@@ -15,7 +17,9 @@ public class UserInfoController {
     @RequestMapping("findAll")
     @ResponseBody
     public List<UserInfo> findAll(){
+
         List<UserInfo> userInfoList = userInfoService.findAll();
+
         return userInfoList;
     }
 
--- gmall-usermanage/src/main/java/com/atguigu/gmall/usermanage/service/impl/UserInfoServiceImpl.java
+++ gmall-usermanage/src/main/java/com/atguigu/gmall/usermanage/service/impl/UserInfoServiceImpl.java
@@ -4,9 +4,11 @@ import com.atguigu.gmall.bean.UserInfo;
 import com.atguigu.gmall.service.UserInfoService;
 import com.atguigu.gmall.usermanage.mapper.UserInfoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserInfoServiceImpl implements UserInfoService {
     @Autowired
     private UserInfoMapper userInfoMapper;
